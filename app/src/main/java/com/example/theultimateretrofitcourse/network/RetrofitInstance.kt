9c566c3cff7226696eb573b4ae6b14e167dcd960
BrunaSBottino <brunasbottino@gmail.com>
2022-06-02 22:52:44 -0300

    Initial commit

---
+++ app/src/main/java/com/example/theultimateretrofitcourse/network/RetrofitInstance.kt
@@ -0,0 +1,16 @@
+package com.example.theultimateretrofitcourse.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+
+    val api : TodoApi by lazy {
+        Retrofit.Builder()
+            .baseUrl("http://jsonplaceholder.typicode.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(TodoApi::class.java)
+    }
+
+}
